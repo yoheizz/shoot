87dcabe1c9bb337b48224bd8b1353c49f8005075
yoheizz <gus.g.fire@gmail.com>
2023-09-15 13:36:48 +0900

    枠変更

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // 定数関係
 const CANVAS_W = 400;
 const CANVAS_H = 400;
+
 const FLAME = 1000/60;
 const BOX_M =100;
 
@@ -14,21 +15,21 @@ ctx.fillStyle = "black";
 ctx.fillRect(0,0,CANVAS_W,CANVAS_H);
 
 // 関数関係
-const rand=(min,max) => {
-    return Math.floor(Math.random()*(max-min+1)+min)
+const rand=(min,max)=>{
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // クラス関係
 class Box{
     constructor(){
-        this.size =2;
+        this.size = 2;
         this.x = rand(0,CANVAS_W);
         this.y = rand(0,CANVAS_H);
         this.vx = 10;
         this.vy = 10;
     }
     drow(){
-        ctx.fillStyle = "red"
+        ctx.fillStyle = "red";
         ctx.fillRect(this.x,this.y,this.size,this.size);
     }
     update(){
@@ -37,11 +38,12 @@ class Box{
         this.z += this.vz;
     }
 };
-
+// とりあえず描画
 const box = [];
 for(let i=0;i<BOX_M;i++)box[i] = new Box();
 for(let i=0;i<BOX_M;i++)box[i].drow();
 
-const Main =() => {
+// メイン処理
+const Main=()=> {
     
 }
