c3aeaef65fb55be61ca57e2fd0e1732c5208c273
yoheizz <gus.g.fire@gmail.com>
2023-09-15 15:07:37 +0900

    player追加

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const CANVAS_W = 400;
 const CANVAS_H = 400;
 
-const FLAME = 1000/60;
+const GAME_S = 1000/60;
 const BOX_M =100;
 
 // canvas系
@@ -11,8 +11,6 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = CANVAS_W;
 canvas.height = CANVAS_H;
-ctx.fillStyle = "black";
-ctx.fillRect(0,0,CANVAS_W,CANVAS_H);
 
 // 関数関係
 const rand=(min,max)=>{
@@ -25,25 +23,78 @@ class Box{
         this.size = 2;
         this.x = rand(0,CANVAS_W);
         this.y = rand(0,CANVAS_H);
-        this.vx = 10;
-        this.vy = 10;
-    }
+        this.vx = 0.1;
+        this.vy = 1;
+    };
     drow(){
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(this.x,this.y,this.size,this.size);
+    };
+    update(){
+        if (this.x>CANVAS_W) {
+            this.x = 0;
+        }else{
+            this.x += this.vx;
+        };
+        
+        if (this.y>CANVAS_H) {
+            this.y = 0;
+        } else {
+            this.y += this.vy;            
+        };
+    };
+};
+
+class player{
+    constructor(){
+        this.size = 20;
+        this.x = CANVAS_W/2;
+        this.y = CANVAS_H - this.size*2;
+        this.vx = 4;
+        this.vy = 0;
+    };
+    drow(){
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = "5";
+        ctx.strokeRect(this.x,this.y,this.size,this.size);
+        ctx.fillStyle = "white";
         ctx.fillRect(this.x,this.y,this.size,this.size);
     }
     update(){
-        this.x += this.vx;
-        this.y += this.vy;
-        this.z += this.vz;
-    }
-};
-// とりあえず描画
+        if (this.x>CANVAS_W) {
+            this.x = 0;
+        }else{
+            this.x += this.vx;
+        };
+        
+        if (this.y>CANVAS_H) {
+            this.y = 0;
+        } else {
+            this.y += this.vy;            
+        };
+    };
+}
+
+//new
 const box = [];
 for(let i=0;i<BOX_M;i++)box[i] = new Box();
-for(let i=0;i<BOX_M;i++)box[i].drow();
+const Player = new player()
+
 
 // メイン処理
-const Main=()=> {
-    
-}
+const loop=()=> {
+    // 移動
+    for(let i=0;i<BOX_M;i++)box[i].update(); 
+    Player.update();
+   
+    //固定
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,CANVAS_W,CANVAS_H);
+    for(let i=0;i<BOX_M;i++)box[i].drow();
+    Player.drow();
+};
+
+//関数呼び出し
+window.onload=()=>{
+    setInterval(loop,GAME_S);
+};
