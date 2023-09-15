449239f8f2439be69629fe84ad109d002e4711f5
yoheizz <gus.g.fire@gmail.com>
2023-09-15 12:08:16 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_W = 800;
+const CANVAS_H = 800;
+const SCREEN_W = 200;
+const SCREEN_H = 200;
+
+canvas.width = SCREEN_W;
+canvas.height = SCREEN_H;
+
